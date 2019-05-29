4742779fbfcc14965f20e36bec8a297c0e543492
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-05-28 17:19:23 -0700

    completed ddd on character item.js

--- src/components/CharacterItem.js
+++ src/components/CharacterItem.js
@@ -2,12 +2,12 @@ import Component from './Component.js';
 
 class CharacterItem extends Component {
     renderTemplate() {
-        // const character = this.props.character;
+        const character = this.props.character;
         return /*html*/ `
-        <li>
-        <h2>46th Earth King</h2>
-        <img src="https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441" alt="46th Earth King">
-        </li>
+            <li>
+                <h2>${character.name}</h2>
+                <img src="${character.photoUrl}" alt="${character.name}">
+            </li>
         `;
     }
 }
