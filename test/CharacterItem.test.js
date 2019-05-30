56156961b4931d00f2cb893d3d675bb583e9ab80
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-05-29 17:12:19 -0700

    fixed test to pass travis

--- test/CharacterItem.test.js
+++ test/CharacterItem.test.js
@@ -16,6 +16,8 @@ test('Rendering template from character data', assert => {
     <li>
         <h2>46th Earth King</h2>
         <img src="https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441" alt="46th Earth King">
+        <a href="#enemies=46th+Earth+King">Enemies</a>
+        <a href="#allies=46th+Earth+King">Allies</a>
     </li>
     `);
 });
